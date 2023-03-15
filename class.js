8fb187bba6e4aa1ef12a56b66fba4cb45cc12ca3
chopra1 <97356733+chopra1@users.noreply.github.com>
2023-03-15 16:09:51 +0530

    Create class.js

---
+++ class.js
@@ -0,0 +1,23 @@
+*class ClassName {
+  constructor() { ... }
+  method_1() { ... }
+  method_2() { ... }
+  method_3() { ... }
+} */
+
+class car {
+    constructor(name, year) {
+        this.name = name;
+        this.year = year;
+    }
+    age() {
+        let date = new Date();
+        return date.getFullYear() - this.year;
+    }
+}
+let myCar1 = new car('ford', 2013);
+let myCar2 = new car('maruti', 2010);
+
+console.log(myCar1.name + " " + myCar1.year);
+console.log(myCar2.name + " " + myCar2.year);
+console.log("My car is " + myCar1.age() + " years old.")
